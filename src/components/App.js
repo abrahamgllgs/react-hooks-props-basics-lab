0202cf3745e8e9c2671327d88705f23f33fb00d0
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-07 00:01:10 -0500

    complete assignemnt

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,21 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
 
 function App() {
+  const user = {
+    name: "Liza",
+    city: "New York",
+    color: "firebrick",
+    bio: "I made this!",
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  };
   return (
-    <div>
+    <div className="App">
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} color={user.color} city={user.city} />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin} />
     </div>
   );
 }
